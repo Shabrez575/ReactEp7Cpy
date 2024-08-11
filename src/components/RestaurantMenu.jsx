f39b384cd9c33d3714a0395291d15d6107d2eeb4
Shabrez575 <77329187+Shabrez575@users.noreply.github.com>
2024-08-11 06:38:36 +0000

    Initial commit

---
+++ src/components/RestaurantMenu.jsx
@@ -0,0 +1,16 @@
+const RestaurantMenu = () => {
+  return (
+    <div>
+      <h1>Biryani Mall</h1>
+      <h2>Menu</h2>
+      <ul>
+        <li>Chicken Biryani</li>
+        <li>Egg Biryani</li>
+        <li>Mutton Biryani</li>
+        <li>Veg Biryani</li>
+      </ul>
+    </div>
+  );
+};
+
+export default RestaurantMenu;
